d1ba12def84f8815f485bb4779e0c0d937ad12e8
Amy Silva <amyambard@icloud.com>
2018-09-20 11:49:30 -0700

    Add search function to user input

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react'
 
 class BookSearch extends Component {
 
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+  this.setState({ query })
+  }
+
   render() {
+    const { books } = this.props
+    const { query } = this.state
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author"/>
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
 
           </div>
         </div>
