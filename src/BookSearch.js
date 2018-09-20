3e137ac70a29affe2f74599aa95bc6d6564fc775
Amy Silva <amyambard@icloud.com>
2018-09-20 11:04:40 -0700

    Remove comments

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -8,9 +8,6 @@ class BookSearch extends Component {
         <div className="search-books-bar">
           <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms, which can be found: https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-            */}
             <input type="text" placeholder="Search by title or author"/>
 
           </div>
